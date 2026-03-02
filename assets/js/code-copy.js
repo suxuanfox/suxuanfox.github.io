d61d42a5b8ea8cedf10d499fdef8df6fa496c79b
suxuanfox <85597781+suxuanfox@users.noreply.github.com>
2026-03-02 16:59:36 +0800

    Create code-copy.js

---
+++ assets/js/code-copy.js
@@ -0,0 +1,7 @@
+function copyCode(btn) {
+  const code = btn.nextElementSibling.innerText;
+  navigator.clipboard.writeText(code).then(() => {
+    btn.innerText = 'Copied!';
+    setTimeout(() => btn.innerText = 'Copy', 1000);
+  });
+}
