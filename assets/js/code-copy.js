0d61f967362b706c03d239eff549ada2d2c85c71
suxuanfox <85597781+suxuanfox@users.noreply.github.com>
2026-03-02 17:11:18 +0800

    Delete assets/js directory

--- assets/js/code-copy.js
+++
@@ -1,7 +0,0 @@
-function copyCode(btn) {
-  const code = btn.nextElementSibling.innerText;
-  navigator.clipboard.writeText(code).then(() => {
-    btn.innerText = 'Copied!';
-    setTimeout(() => btn.innerText = 'Copy', 1000);
-  });
-}
